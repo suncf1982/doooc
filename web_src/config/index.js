a4a94f90144f48667aca5ae3b8c49092e2147cb5
suncf <905517073@qq.com>
2018-08-28 17:25:19 +0800

    整理目录

--- web_src/config/index.js
+++ web_src/config/index.js
@@ -59,7 +59,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'static/assets',
 
     /**
      * You can set by youself according to actual condition
